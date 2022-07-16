5df972307ed086c8e28825b9a4754deb37c48139
Francisko <errasti007@yandex.ru>
2022-07-16 12:16:35 +0300

    Initial commit

---
+++ proj_01/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Введите координаты x ");
+int xinput = int.Parse(Console.ReadLine());
+
+Console.Write("Введите координаты y ");
+int yinput = int.Parse(Console.ReadLine());
+
+int getQuarter ( int x, int y )
+{
+    if ( x > 0 && y > 0)
+    {
+        return 1;
+    }
+    if ( x < 0 && y > 0)
+    {
+        return 2;
+    }
+    if ( x < 0 && y < 0)
+    {
+        return 3;
+    }
+    if ( x > 0 && y < 0)
+    {
+        return 4;
+    }
+return 0;
+}
+
+int result = getQuarter(xinput , yinput );
+Console.WriteLine(result);
