f6e5ec13f2050a84e2d68e8f58912c412c28b615
Francisko <errasti007@yandex.ru>
2022-07-16 12:52:06 +0300

    Initial commit

---
+++ proj_02/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите комер четверти: ");
+int quarter = int.Parse(Console.ReadLine());
+
+string  XY ( int number )
+{
+    if( number == 1 )
+    {
+        return "X > 0 ; Y > 0";
+    }
+    if ( number == 2 )
+    {
+        return "X < 0 ; Y > 0";
+    }
+    if ( number == 3 )
+    {
+        return "X < 0 ; Y < 0";
+    }
+    if ( number == 4 )
+    {
+        return "X > 0 ; Y < 0";
+    }
+    return "Нет такой четверти";
+}
+
+Console.WriteLine(XY(quarter));
+
+
