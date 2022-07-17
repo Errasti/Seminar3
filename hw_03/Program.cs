9a5da4a7146305f58c65f003d0072bc52bcf38e0
Francisko <errasti007@yandex.ru>
2022-07-17 10:46:09 +0300

    Initial Commit

---
+++ hw_03/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число N: ");
+int cube = int.Parse(Console.ReadLine());
+
+void getCube ( int n )
+{
+    int count = 1;
+    if ( n <= 0 )
+    {
+        Console.WriteLine("Введите положительное N");
+    }
+    else if ( n > 0)
+    {
+        while ( count <= n )
+        {
+            int result = count * count * count;
+            Console.Write(result + " ");
+            count ++;
+        }
+    }
+}
+getCube(cube);
